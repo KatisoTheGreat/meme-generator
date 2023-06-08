f9e5eed1743459347b0a9a57968a85b7f6ddf949
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-08 20:11:10 +0200

    update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,11 +3,11 @@ import React from "react";
 function Meme() {
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form-input"></input>
                 <input type="text" className="form-input"></input>
                 <button className="form-button">Get a new meme image 🖼</button>
-            </form>
+            </div>
         </main>
     )
 }
