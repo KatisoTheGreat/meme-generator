6e55cf36232608877611a8c158c05d60c00256c9
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-08 19:32:22 +0200

    added form elements on component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../src/components/Header";
+import Meme from "../src/components/Meme"
 
 function App() {
     return(
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
